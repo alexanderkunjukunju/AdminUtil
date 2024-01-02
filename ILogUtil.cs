3e1ec694ccb1cba09667332acac674966dc6537b
LIVINGROOM-PC\Alex <akunjukunju@gmail.com>
2024-01-01 21:40:55 -0500

    Initial Commit

---
+++ ILogUtil.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AdminUtil
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ILogUtil
+    {
+        [OperationContract]
+        bool LogMessage(string userid, char msgtype, string message, out string err_msg);
+        [OperationContract]
+        messsagesTable[] GetMessages(string userid, out string err_msg);
+        [OperationContract]
+        messsagesTable[] GetMessagesDateRange(string userid, DateTime fromDate, DateTime toDate, out string errMsg);
+        [OperationContract]
+        messsagesTable[] GetMessagesType(string userid, char type, out string errMsg);
+        [OperationContract]
+        messsagesTable[] GetMessagesTypeDateRange(string userid, char type, DateTime fromDate, DateTime toDate, out string errMsg);
+        [OperationContract]
+        messsagesTable[] GetMessagesUser(string userid, string user, out string errMsg);
+        [OperationContract]
+        messsagesTable[] GetMessagesUserDateRange(string userid, string user, DateTime fromDate, DateTime toDate, out string errMsg);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class messsagesTable
+    {
+        char exceptionType;
+        string errMsg;
+        string userID;
+        DateTime execeptionTime;
+
+        [DataMember]
+        public char ExceptionType
+        {
+            get { return exceptionType; }
+            set { exceptionType = value; }
+        }
+
+        [DataMember]
+        public string ErrorMessage
+        {
+            get { return errMsg; }
+            set { errMsg = value; }
+        }
+        [DataMember]
+        public string UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        [DataMember]
+        public DateTime ExeceptionTime
+        {
+            get { return execeptionTime; }
+            set { execeptionTime = value; }
+        }
+    }
+}
---
+++ ILogUtil.svc.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AdminUtil
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class LogUtil : ILogUtil
+    {
+        public bool LogMessage(string userid, char msgtype, string message, out string err_msg)
+        {
+            /*            OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        return dba.LogMessage(msgtype, message, userid, out err_msg);
+            */
+            err_msg = "";
+            return false;
+
+        }
+
+        public messsagesTable[] GetMessages(string userid, out string err_msg)
+        {
+            /*            List<messsagesTable> messages;
+
+                        OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        bool retVal = dba.getMessages(out messages, out err_msg);
+
+                        if (retVal)
+                            return messages.ToArray();
+            */
+            err_msg = "";
+            return null;
+        }
+
+        public messsagesTable[] GetMessagesDateRange(string userid, DateTime fromDate, DateTime toDate, out string errMsg)
+        {
+            /*            List<messsagesTable> messages;
+
+                        if (fromDate > toDate)
+                        {
+                            DateTime temp = fromDate;
+                            fromDate = toDate;
+                            toDate = temp;
+                        }
+
+                        OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        bool retVal = dba.getMessagesDateRange(fromDate, toDate, out messages, out errMsg);
+
+                        if (retVal)
+                            return messages.ToArray();
+            */
+            errMsg = "";
+            return null;
+        }
+
+        public messsagesTable[] GetMessagesType(string userid, char type, out string errMsg)
+        {
+            /*            List<messsagesTable> messages;
+
+                        OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        bool retVal = dba.getMessagesType(type, out messages, out errMsg);
+
+                        if (retVal)
+                            return messages.ToArray();
+            */
+            errMsg = "";
+            return null;
+        }
+
+        public messsagesTable[] GetMessagesTypeDateRange(string userid, char type, DateTime fromDate, DateTime toDate, out string errMsg)
+        {
+            /*            List<messsagesTable> messages;
+
+                        if (fromDate > toDate)
+                        {
+                            DateTime temp = fromDate;
+                            fromDate = toDate;
+                            toDate = temp;
+                        }
+
+                        OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        bool retVal = dba.getMessagesTypeDateRange(type, fromDate, toDate, out messages, out errMsg);
+
+                        if (retVal)
+                            return messages.ToArray();
+            */
+            errMsg = "";
+            return null;
+        }
+
+        public messsagesTable[] GetMessagesUser(string userid, string user, out string errMsg)
+        {
+            /*            List<messsagesTable> messages;
+
+                        OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                        bool retVal = dba.getMessagesUser(user, out messages, out errMsg);
+
+                        if (retVal)
+                            return messages.ToArray();
+            */
+            errMsg = "";
+            return null;
+        }
+
+
+        public messsagesTable[] GetMessagesUserDateRange(string userid, string user, DateTime fromDate, DateTime toDate, out string errMsg)
+        {
+            /*           List<messsagesTable> messages;
+
+                       if (fromDate > toDate)
+                       {
+                           DateTime temp = fromDate;
+                           fromDate = toDate;
+                           toDate = temp;
+                       }
+
+                       OraDBLogMessageAccess dba = new OraDBLogMessageAccess();
+
+                       bool retVal = dba.getMessagesUserDateRange(user, fromDate, toDate, out messages, out errMsg);
+
+                       if (retVal)
+                           return messages.ToArray();
+           */
+            errMsg = "";
+            return null;
+        }
+    }
+}
